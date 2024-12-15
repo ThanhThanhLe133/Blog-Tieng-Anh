b6dfc96a4abeacd710d426a13ef3afa0804147f4
Ocleng <151450732+lpmn374@users.noreply.github.com>
2024-12-15 08:41:51 +0700

    Create updateAccount.php

---
+++ User/CaiDatTaiKhoan/updateAccount.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include "../conn.php";
+$user_id = $_SESSION['user_id'];
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["username"]) && isset($_POST["email"])) {
+    $strSQL = "UPDATE admin SET first_name='$firstname',last_name='$lastname', username='$username', email='$email' where username = '$user_id'";
+
+    $result = $conn->query($strSQL);
+
+    if ($result ===TRUE) {
+        $_SESSION['user_logged_in'] = true;
+        $_SESSION['user_id'] = $_POST["username"];
+        echo "Cập nhật thông tin thành công!";
+    } else {
+        echo "Không thể cập nhật thông tin. Vui lòng thử lại.";
+    }
+}
+
+$conn->close();
+?>
