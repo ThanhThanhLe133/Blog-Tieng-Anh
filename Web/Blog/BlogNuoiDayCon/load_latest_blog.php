f5160098152221b64299d4fff9a907737c8e8132
Ocleng <151450732+lpmn374@users.noreply.github.com>
2024-12-17 20:11:39 +0700

    fix lỗi link

--- Web/Blog/BlogNuoiDayCon/load_latest_blog.php
+++ Web/Blog/BlogNuoiDayCon/load_latest_blog.php
@@ -10,7 +10,7 @@ if ($result->num_rows > 0) {
         $blog_id = $row['blog_id'];
         $html .= "
            <li class='mb-2'>
-                <a class='hover:underline' href='blog_detail.php?blog_id={$row['blog_id']}'>{$row['title']}</a>
+                <a class='hover:underline' href='../blogDetail/index.html?blog_id={$row['blog_id']}'>{$row['title']}</a>
             </li>";
     }
     echo $html;
