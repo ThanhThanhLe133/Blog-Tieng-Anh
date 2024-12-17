d5c5cc7b022e413967b0733230457ba9e8ec407b
thanh thanh le <thanhthanh.lethi133@gmail.com>
2024-12-17 13:05:06 +0700

    fix lỗi cài đặt tài khoản admin

--- Admin/CaiDatTaiKhoan/updateAccount.php
+++ Admin/CaiDatTaiKhoan/updateAccount.php
@@ -8,12 +8,11 @@ $lastname = $_POST["lastname"];
 $username = $_POST["username"];
 $email = $_POST["email"];
 if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["username"]) && isset($_POST["email"])) {
-    $strSQL = "UPDATE admin SET first_name='$firstname',last_name='$lastname', username='$username', email='$email' where username = '$username'";
+    $strSQL = "UPDATE admin SET first_name='$firstname',last_name='$lastname', username='$username', email='$email' where username = '$user_name'";
 
     $result = $conn->query($strSQL);
 
     if ($result ===TRUE) {
-        $_SESSION['admin_logged_in'] = true;
         $_SESSION['user_name'] = $_POST["username"];
         echo "Cập nhật thông tin thành công!";
     } else {
