2096ac13d62a2b387fc07f212259492364904cc6
thanh thanh le <thanhthanh.lethi133@gmail.com>
2024-12-17 14:03:55 +0700

    update style in blog + fix icon

--- Web/Blog/BlogTinTucSuKien/BlogDetail/load_prev_next.php
+++ Web/Blog/BlogTinTucSuKien/BlogDetail/load_prev_next.php
@@ -6,15 +6,15 @@ $prev_sql = "SELECT * FROM blogs WHERE category_id=2 AND blog_id < $blog_id ORDE
 $result_prev = $conn->query($prev_sql);
 if ($result_prev->num_rows > 0) {
     $row=$result_prev->fetch_assoc();
-    $html.= "<a class='text-blue-600' href='index.html?blog_id={$row['blog_id']}'>PREVIOUS</a>
-                        <h3 class='text-xl font-semibold'>{$row['title']}</h3>";
+    $html.= "<div><a class='text-blue-600' href='index.html?blog_id={$row['blog_id']}'>PREVIOUS</a>
+                        <h3 class='font-semibold'>{$row['title']}</h3></div>";
 }
 $next_sql = "SELECT * FROM blogs WHERE blog_id > $blog_id ORDER BY blog_id ASC LIMIT 1";
 $result_next = $conn->query($next_sql);
 if ($result_next->num_rows > 0) {
     $row=$result_next->fetch_assoc();
-    $html.= "<a class='text-blue-600' href='index.html?blog_id={$row['blog_id']}'>NEXT</a>
-    <h3 class='text-xl font-semibold'>{$row['title']}</h3>";
+    $html.= "<div><a class='text-blue-600' href='index.html?blog_id={$row['blog_id']}'>NEXT</a>
+    <h3 class='font-semibold'>{$row['title']}</h3></div>";
 }
 echo $html;
 $conn->close();
